4ca356503b95c1b801c1f05fdf01dcfe9e733b33
Bulat <sadikovba@gmail.com>
2018-03-12 18:51:58 +0300

    Initial

---
+++ header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+
+	<title>Haier</title>
+
+	<!-- favocons -->
+	<link rel="apple-touch-icon" sizes="120x120" href="favicons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+	<link rel="manifest" href="favicons/manifest.json">
+	<link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="shortcut icon" href="favicons/favicon.ico">
+	<meta name="msapplication-config" content="favicons/browserconfig.xml">
+	<meta name="theme-color" content="#ffffff">
+	<!-- favocons -->
+
+	<!-- og -->
+	<meta property="og:title" content="Шаблон проекта">
+	<meta property="og:description" content="Описание проекта">
+	<meta property="og:type" content="website">
+	<meta property="og:image" content="/img/og.jpg">
+	<!-- og -->
+
+	<link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+
+<section class="header-sec">
+	<div class="header container">
+		<div class="header__logo">
+			<img src="img/logo-top.svg" alt="">
+		</div>
+		<nav class="header__nav">
+			<a href="#">О магазине</a>
+			<a href="#">Преимущества</a>
+			<a href="#">3d тур</a>
+			<a href="#">Как добраться</a>
+		</nav>
+		<div class="header__contact">
+			<span>Магазин в Набережных Челнах</span>
+			<a href="tel:88552910107">8 (8552) 91-01-07 </a>
+		</div>
+	</div>
+</section>
---
+++ index.php
@@ -0,0 +1,20 @@
+﻿<?php include('header.php'); ?>
+
+<section class="big-head-sec">
+	<div class="big-head container">
+		<div class="big-head__title">
+			<p class="h1">Посетите первый фирменный магазин Haier в России!</p>
+			<span>У нас вы найдете более 100 моделей бытовой техники и электроники для дома или квартиры по разумным ценам.</span>
+		</div>
+		<div class="big-head__contact">
+			<div class="big-head__contact-info">
+				<p>Торговый Центр Сити Молл</p>
+				<span>Набережные Челны, проспект Чулман, 87А</span>
+				<span>10:00 - 22:00</span>
+			</div>
+			<a href="#" class="btn"><i class="i-geo"></i><span>Как добраться</span></a>
+		</div>
+	</div>
+</section>
+
+<?php include('footer.php'); ?>
